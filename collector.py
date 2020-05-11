cb65f5db9eb31f06aab5a120867495f4e1c7c844
seallard <seallard95@gmail.com>
2020-05-11 09:50:51 +0200

    Collect diamonds closest to the base

--- collector.py
+++ collector.py
@@ -19,7 +19,7 @@ def main():
 
         while collected_diamonds < 5:
 
-            diamond = n_closest_diamonds(bot_name, 1)[0]
+            diamond = n_closest_diamonds(base, 1)[0]
             diamond_value = diamond['properties']['points']
 
             if diamond_value + collected_diamonds > 5:
